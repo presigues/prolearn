b5b3978e63889a0348a02bfa35e6c25e53b4c316
presigues <132411345+presigues@users.noreply.github.com>
2024-07-14 13:16:43 -0400

    website now auto generates questions

--- answerhandler.js
+++ answerhandler.js
@@ -1,15 +1,20 @@
-function getRandomIntforElement(min, max) {
+function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
-  let Element = getRandomIntforElement(1,7)
-  elementword;
+elementword;
   element;
   elementend;
+  wordw;
 
-  switch(Element){
+  function ElementSelection(){
+
+  let Element = getRandomInt(1,7)
+  
+
+  switch(Element){ // chose element 
     case 1:
     elementword = "Heading 1";
     element = "<h1>";
@@ -53,3 +58,63 @@ function getRandomIntforElement(min, max) {
     console.log("Element Chosen : " + elementword);
     break;
   }
+}
+
+function WordSelection(){
+
+let Word = getRandomInt(1,10)
+
+
+switch(Word){
+    case 1:
+        wordw = "Hello World!";
+        console.log("Word Chosen : " + wordw);
+        break;
+        case 2:
+            wordw = "Hack Club!";
+        console.log("Word Chosen : " + wordw);
+            break;
+            case 3:
+                wordw = "Welcome to the World of CODE";
+        console.log("Word Chosen : " + wordw);
+                break;
+                case 4:
+                    wordw = "Would you Rather?...";
+        console.log("Word Chosen : " + wordw);
+                    break;
+                    case 5:
+                        wordw = "What is your Name?";
+        console.log("Word Chosen : " + wordw);
+                        break;
+                        case 6:
+                            wordw = "html is very cool";
+        console.log("Word Chosen : " + wordw);
+                            break;
+                            case 7:
+                                wordw = "First Robotics Competition!";
+        console.log("Word Chosen : " + wordw);
+                                break;
+                                case 8:
+                                    wordw = "FIRST!";
+        console.log("Word Chosen : " + wordw);
+                                    break;
+                                    case 9:
+                                        wordw = "Welcome to my Website";
+        console.log("Word Chosen : " + wordw);
+                                        break;
+                                        case 10:
+                                            wordw = "Coding is Cool";
+        console.log("Word Chosen : " + wordw);
+                                            break;
+}
+}
+
+function printFinal(){
+    console.log("Write the Sentence : " + wordw + " as a " + elementword);
+}
+
+function comp(){
+    ElementSelection();
+    WordSelection();
+    printFinal();
+}
